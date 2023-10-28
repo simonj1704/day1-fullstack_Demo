222413a8a77f9c58d7e12534623c9ac8d9675cc5
simonj1704 <simo915m@stud.kea.dk>
2023-10-28 12:10:08 +0200

    commit

--- src/test/java/day1/day1demo2/Day1Demo2ApplicationTests.java
+++ src/test/java/day1/day1demo2/Day1Demo2ApplicationTests.java
@@ -1,10 +1,28 @@
 package day1.day1demo2;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Day1Demo2ApplicationTests {
+
+	@BeforeAll
+	static void setUp() {
+		//Setup system variables for DB connection
+		System.setProperty("JDBC_DATABASE_URL","jdbc:mysql://localhost:3306/players");
+		System.setProperty("JDBC_USERNAME","root");
+		System.setProperty("JDBC_PASSWORD","password");
+	}
+
+	@AfterAll
+	static void tearDown() {
+		//Remove system variables for DB connection
+		System.clearProperty("JDBC_DATABASE_URL");
+		System.clearProperty("JDBC_USERNAME");
+		System.clearProperty("JDBC_PASSWORD");
+	}
 
 	@Test
 	void contextLoads() {
